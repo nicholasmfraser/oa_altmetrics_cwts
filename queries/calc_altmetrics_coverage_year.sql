6a0eb46034153bd5f7a949ff99c346c7da521c18
Fraser <frasernm@VUW.leidenuniv.nl>
2020-01-22 10:51:41 +0100

    wip

--- queries/calc_altmetrics_coverage_year.sql
+++ queries/calc_altmetrics_coverage_year.sql
@@ -3,12 +3,13 @@ SET NOCOUNT ON;
 /* Measure overall coverage of each altmetric indicator per year */
 SELECT
 	t1.year,
-	CAST(SUM(CASE WHEN t2.count_blog = 0 THEN 0 ELSE 1 END) AS DECIMAL) / CAST(COUNT(*) AS DECIMAL) AS blogs,
-	CAST(SUM(CASE WHEN t2.count_facebook = 0 THEN 0 ELSE 1 END) AS DECIMAL) / CAST(COUNT(*) AS DECIMAL) AS facebook,
-	CAST(SUM(CASE WHEN t2.count_news = 0 THEN 0 ELSE 1 END) AS DECIMAL) / CAST(COUNT(*) AS DECIMAL) AS news,
-	CAST(SUM(CASE WHEN t2.count_policy = 0 THEN 0 ELSE 1 END) AS DECIMAL) / CAST(COUNT(*) AS DECIMAL) AS policies,
-	CAST(SUM(CASE WHEN t2.count_twitter = 0 THEN 0 ELSE 1 END) AS DECIMAL) / CAST(COUNT(*) AS DECIMAL) AS twitter,
-	CAST(SUM(CASE WHEN t2.count_wikipedia = 0 THEN 0 ELSE 1 END) AS DECIMAL) / CAST(COUNT(*) AS DECIMAL) AS wikipedia
+	COUNT(*) AS items,
+	SUM(CASE WHEN t2.count_blog = 0 THEN 0 ELSE 1 END) AS blogs,
+	SUM(CASE WHEN t2.count_facebook = 0 THEN 0 ELSE 1 END) AS facebook,
+	SUM(CASE WHEN t2.count_news = 0 THEN 0 ELSE 1 END) AS news,
+	SUM(CASE WHEN t2.count_policy = 0 THEN 0 ELSE 1 END) AS policies,
+	SUM(CASE WHEN t2.count_twitter = 0 THEN 0 ELSE 1 END) AS twitter,
+	SUM(CASE WHEN t2.count_wikipedia = 0 THEN 0 ELSE 1 END) AS wikipedia
 FROM
 	userdb_frasernm.dbo.wos_items t1
 INNER JOIN
