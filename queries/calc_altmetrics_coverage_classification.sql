f49573257897378b044a4b2f50b23195e741afc0
Fraser <frasernm@VUW.leidenuniv.nl>
2020-01-22 12:08:21 +0100

    wip

--- queries/calc_altmetrics_coverage_classification.sql
+++ queries/calc_altmetrics_coverage_classification.sql
@@ -2,29 +2,25 @@ SET NOCOUNT ON;
 
 /* Calculate coverage of each altmetric indicator in each subject classification */
 SELECT
-	t4.LR_main_field AS classification,
-	CAST(SUM(t4.weight) AS DECIMAL) AS items,
-	SUM(CASE WHEN t3.count_blog = 0 THEN 0 ELSE 1 * t4.weight END) AS blogs,
-	SUM(CASE WHEN t3.count_facebook = 0 THEN 0 ELSE 1 * t4.weight END) AS facebook,
-	SUM(CASE WHEN t3.count_news = 0 THEN 0 ELSE 1 * t4.weight END) AS news,
-	SUM(CASE WHEN t3.count_policy = 0 THEN 0 ELSE 1 * t4.weight END) AS policies,
-	SUM(CASE WHEN t3.count_twitter = 0 THEN 0 ELSE 1 * t4.weight END) AS twitter,
-	SUM(CASE WHEN t3.count_wikipedia = 0 THEN 0 ELSE 1 * t4.weight END) AS wikipedia
+	t3.LR_main_field AS classification,
+	CAST(SUM(t3.weight) AS DECIMAL) AS items,
+	CAST(SUM(CASE WHEN t2.count_blog = 0 THEN 0 ELSE 1 * t3.weight END) AS DECIMAL) AS blogs,
+	CAST(SUM(CASE WHEN t2.count_facebook = 0 THEN 0 ELSE 1 * t3.weight END) AS DECIMAL) AS facebook,
+	CAST(SUM(CASE WHEN t2.count_news = 0 THEN 0 ELSE 1 * t3.weight END) AS DECIMAL) AS news,
+	CAST(SUM(CASE WHEN t2.count_policy = 0 THEN 0 ELSE 1 * t3.weight END) AS DECIMAL) AS policies,
+	CAST(SUM(CASE WHEN t2.count_twitter = 0 THEN 0 ELSE 1 * t3.weight END) AS DECIMAL) AS twitter,
+	CAST(SUM(CASE WHEN t2.count_wikipedia = 0 THEN 0 ELSE 1 * t3.weight END) AS DECIMAL) AS wikipedia
 FROM
-	userdb_frasernm.dbo.unpaywall_classification_pmccor t1
+	userdb_frasernm.dbo.wos_items t1
 INNER JOIN
-	userdb_frasernm.dbo.wos_items t2
+	userdb_frasernm.dbo.altmetric_counts t2
 ON
 	t1.doi = t2.doi
 INNER JOIN
-	userdb_frasernm.dbo.altmetric_counts t3
+	userdb_frasernm.dbo.wos_classification t3
 ON
-	t2.doi = t3.doi
-INNER JOIN
-	userdb_frasernm.dbo.wos_classification t4
-ON
-	t2.ut = t4.ut
+	t1.ut = t3.ut
 GROUP BY
-	t4.LR_main_field, t1.closed
+	t3.LR_main_field
 ORDER BY 
-	t4.LR_main_field, t1.closed;
+	t3.LR_main_field;
--- queries/calc_altmetrics_coverage_classification_oa.sql
+++ queries/calc_altmetrics_coverage_classification_oa.sql
@@ -1,16 +1,16 @@
 SET NOCOUNT ON;
 
-/* Calculate coverage of each altmetric indicator in OA versus non-OA publications grouped by subject classification*/
+/* Calculate coverage of each altmetric indicator in each subject classification */
 SELECT
-	t4.LR_main_field,
+	t4.LR_main_field AS classification,
 	CASE WHEN t1.closed = 1 THEN 'closed' ELSE 'open' END AS type,
-	CAST(SUM(t4.weight) AS DECIMAL) AS n_items,
-	SUM(CASE WHEN t3.count_blog = 0 THEN 0 ELSE 1 END) AS n_blog,
-	SUM(CASE WHEN t3.count_facebook = 0 THEN 0 ELSE 1 END) AS n_facebook,
-	SUM(CASE WHEN t3.count_news = 0 THEN 0 ELSE 1 END) AS n_news,
-	SUM(CASE WHEN t3.count_policy = 0 THEN 0 ELSE 1 END) AS n_policy,
-	SUM(CASE WHEN t3.count_twitter = 0 THEN 0 ELSE 1 END) AS n_twitter,
-	SUM(CASE WHEN t3.count_wikipedia = 0 THEN 0 ELSE 1 END) AS n_wikipedia
+	CAST(SUM(t4.weight) AS DECIMAL) AS items,
+	CAST(SUM(CASE WHEN t3.count_blog = 0 THEN 0 ELSE 1 * t4.weight END) AS DECIMAL) AS blogs,
+	CAST(SUM(CASE WHEN t3.count_facebook = 0 THEN 0 ELSE 1 * t4.weight END) AS DECIMAL) AS facebook,
+	CAST(SUM(CASE WHEN t3.count_news = 0 THEN 0 ELSE 1 * t4.weight END) AS DECIMAL) AS news,
+	CAST(SUM(CASE WHEN t3.count_policy = 0 THEN 0 ELSE 1 * t4.weight END) AS DECIMAL) AS policies,
+	CAST(SUM(CASE WHEN t3.count_twitter = 0 THEN 0 ELSE 1 * t4.weight END) AS DECIMAL) AS twitter,
+	CAST(SUM(CASE WHEN t3.count_wikipedia = 0 THEN 0 ELSE 1 * t4.weight END) AS DECIMAL) AS wikipedia
 FROM
 	userdb_frasernm.dbo.unpaywall_classification_pmccor t1
 INNER JOIN
