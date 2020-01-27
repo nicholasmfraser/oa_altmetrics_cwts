24b042bd25f215b9b85aadc35da48fe0a2dee7ab
Fraser <frasernm@VUW.leidenuniv.nl>
2020-01-27 15:27:04 +0100

    wip

--- queries/calc_altmetrics_coverage_country.sql
+++ queries/calc_altmetrics_coverage_country.sql
@@ -4,12 +4,18 @@ SET NOCOUNT ON;
 SELECT
 	t3.country AS country,
 	CAST(SUM(t3.country_weight) AS DECIMAL) AS items,
-	CAST(SUM(CASE WHEN t2.count_blog = 0 THEN 0 ELSE 1 * t3.country_weight END) AS DECIMAL) AS blogs,
-	CAST(SUM(CASE WHEN t2.count_facebook = 0 THEN 0 ELSE 1 * t3.country_weight END) AS DECIMAL) AS facebook,
-	CAST(SUM(CASE WHEN t2.count_news = 0 THEN 0 ELSE 1 * t3.country_weight END) AS DECIMAL) AS news,
-	CAST(SUM(CASE WHEN t2.count_policy = 0 THEN 0 ELSE 1 * t3.country_weight END) AS DECIMAL) AS policies,
-	CAST(SUM(CASE WHEN t2.count_twitter = 0 THEN 0 ELSE 1 * t3.country_weight END) AS DECIMAL) AS twitter,
-	CAST(SUM(CASE WHEN t2.count_wikipedia = 0 THEN 0 ELSE 1 * t3.country_weight END) AS DECIMAL) AS wikipedia
+	CAST(SUM(CASE WHEN t2.count_blog = 0 THEN 0 ELSE 1 END) AS DECIMAL) AS blogs,
+	CAST(SUM(CASE WHEN t2.count_facebook = 0 THEN 0 ELSE 1 END) AS DECIMAL) AS facebook,
+	CAST(SUM(CASE WHEN t2.count_news = 0 THEN 0 ELSE 1 END) AS DECIMAL) AS news,
+	CAST(SUM(CASE WHEN t2.count_policy = 0 THEN 0 ELSE 1 END) AS DECIMAL) AS policies,
+	CAST(SUM(CASE WHEN t2.count_twitter = 0 THEN 0 ELSE 1 END) AS DECIMAL) AS twitter,
+	CAST(SUM(CASE WHEN t2.count_wikipedia = 0 THEN 0 ELSE 1  END) AS DECIMAL) AS wikipedia,
+	CAST(SUM(CASE WHEN t2.count_blog = 0 THEN 0 ELSE 1 * t3.country_weight END) AS DECIMAL) AS blogs_weighted,
+	CAST(SUM(CASE WHEN t2.count_facebook = 0 THEN 0 ELSE 1 * t3.country_weight END) AS DECIMAL) AS facebook_weighted,
+	CAST(SUM(CASE WHEN t2.count_news = 0 THEN 0 ELSE 1 * t3.country_weight END) AS DECIMAL) AS news_weighted,
+	CAST(SUM(CASE WHEN t2.count_policy = 0 THEN 0 ELSE 1 * t3.country_weight END) AS DECIMAL) AS policies_weighted,
+	CAST(SUM(CASE WHEN t2.count_twitter = 0 THEN 0 ELSE 1 * t3.country_weight END) AS DECIMAL) AS twitter_weighted,
+	CAST(SUM(CASE WHEN t2.count_wikipedia = 0 THEN 0 ELSE 1 * t3.country_weight END) AS DECIMAL) AS wikipedia_weighted
 FROM
 	userdb_frasernm.dbo.wos_items t1
 INNER JOIN
