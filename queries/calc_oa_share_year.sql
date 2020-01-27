8d976f1cfec2abb680499b50975ac1da27df9b1f
Fraser <frasernm@VUW.leidenuniv.nl>
2020-01-27 12:16:55 +0100

    wip

--- queries/calc_oa_share_year.sql
+++ queries/calc_oa_share_year.sql
@@ -1,12 +1,16 @@
 SET NOCOUNT ON;
 
-/* Calculate the share of publications that are open versus closed */
+/* Calculate the share of publications in each open access categorz */
 SELECT
 	t2.year,
 	SUM(t1.closed) AS closed,
-	COUNT(*) - SUM(t1.closed) AS oa
+	COUNT(*) - SUM(t1.closed) AS [open],
+	SUM(t1.gold) AS gold,
+	SUM(t1.hybrid) AS hybrid,
+	SUM(t1.bronze) AS bronze,
+	SUM(t1.green) AS green
 FROM
-	userdb_frasernm.dbo.unpaywall_classification_nopmccor t1
+	userdb_frasernm.dbo.unpaywall_classification_pmccor t1
 INNER JOIN
 	userdb_frasernm.dbo.wos_items t2
 ON
