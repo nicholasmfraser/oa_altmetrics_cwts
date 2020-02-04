37e76f14cb5dceea7715fcb74abb7c58bc923743
Fraser <frasernm@VUW.leidenuniv.nl>
2020-02-04 17:00:39 +0100

    wip

---
+++ queries/calc_blogs_min_timediff_oa.sql
@@ -0,0 +1,30 @@
+SET NOCOUNT ON;
+
+/* Calculate the mean time difference between an article being published
+and receiving its first mention on blogs, grouped by OA vs non-OA articles (2016 only) */
+SELECT DISTINCT
+	s1.type,
+	PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY s1.min_timediff) OVER (PARTITION BY s1.type) AS median_min_timediff
+FROM (
+	SELECT
+		t1.doi,
+		CASE WHEN t1.closed = 1 THEN 'closed' ELSE 'open' END AS type,
+		MIN(DATEDIFF(dd, t3.citation_pubdate, t4.posted_on)) AS min_timediff
+	FROM
+		userdb_frasernm.dbo.unpaywall_classification_pmccor t1
+	INNER JOIN
+		userdb_frasernm.dbo.wos_items t2
+	ON
+		t1.doi = t2.doi
+	INNER JOIN
+		altmetric_2017oct.dbo.main t3
+	ON
+		t1.doi = t3.citation_doi
+	INNER JOIN
+		altmetric_2017oct.dbo.blog t4
+	ON
+		t3.altmetric_id = t4.altmetric_id
+	WHERE
+		YEAR(t3.citation_pubdate) = 2016
+	GROUP BY
+		t1.doi, t1.closed) AS s1;
---
+++ queries/calc_policy_timediff_oa.sql
@@ -0,0 +1,26 @@
+SET NOCOUNT ON;
+
+/* Calculate the time difference between an article being published
+and being mentioned in policy documents, grouped by OA vs non-OA articles (2017 only) */
+SELECT
+	CASE WHEN t1.closed = 1 THEN 'closed' ELSE 'open' END AS type,
+	DATEDIFF(dd, t3.citation_pubdate, t4.posted_on) as timediff,
+	COUNT(*) AS n_items
+FROM
+	userdb_frasernm.dbo.unpaywall_classification_pmccor t1
+INNER JOIN
+	userdb_frasernm.dbo.wos_items t2
+ON
+	t1.doi = t2.doi
+INNER JOIN
+	altmetric_2017oct.dbo.main t3
+ON
+	t1.doi = t3.citation_doi
+INNER JOIN
+	altmetric_2017oct.dbo.policy t4
+ON
+	t3.altmetric_id = t4.altmetric_id
+WHERE
+	YEAR(t3.citation_pubdate) = 2016
+GROUP BY
+	DATEDIFF(dd, t3.citation_pubdate, t4.posted_on), t1.closed;
